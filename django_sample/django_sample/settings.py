0c0a84beaf112438dce02ac832a7fc017388877b
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2025-03-28 11:33:45 -0300

    allowed hosts

--- django_sample/django_sample/settings.py
+++ django_sample/django_sample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "h2ff#tee@ypp*t7(jy*42gp6-en#p5*2za*yf65$3pzquhx368"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172-105-23-184.ip.linodeusercontent.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['172-105-23-184.ip.linodeusercontent.com', 'localhost', '127.0.0.1', '172.105.23.184']
 
 
 # Application definition
