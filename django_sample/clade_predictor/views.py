f176e8375f536347c96e1f59618337e36fdde69f
Gustavo Sganzerla <77517507+gustavsganzerla@users.noreply.github.com>
2025-04-04 09:21:45 -0300

    added second model logics

--- django_sample/clade_predictor/views.py
+++ django_sample/clade_predictor/views.py
@@ -20,10 +20,19 @@ def generate_kmers(sequence, k):
 
 # Create your views here.
 def home(request):
-    loaded_model = xgb.XGBClassifier()
+    model_clade = xgb.XGBClassifier()
+    model_subclade = xgb.XGBClassifier()
     output = []
+    n_seqs = 0
 
-    loaded_model.load_model('/var/www/django_app/django_sample/models/xgb_model.json')
+    model_clade.load_model('/var/www/django_app/django_sample/models/xgb_model.json')
+    model_subclade.load_model('/var/www/django_app/django_sample/models/xgb_model_subclade.json')
+
+    #model_clade.load_model('/Users/gustavosganzerla/mpxv_clade_prediction/django_sample/models/xgb_model.json')
+    #model_subclade.load_model('/Users/gustavosganzerla/mpxv_clade_prediction/django_sample/models/xgb_model_subclade.json')
+
+
+    
     if request.method == 'POST':
         form = genomeForm(request.POST)
         all_kmers = [''.join(kmer) for kmer in itertools.product('ATCG', repeat=3)]
@@ -36,28 +45,41 @@ def home(request):
             if genome_text:
                
                 for record in SeqIO.parse(StringIO(genome_text), 'fasta'):
+                   n_seqs +=1
                    kmer_counts = generate_kmers(record.seq, 3)
                    features = [kmer_counts.get(kmer, 0) for kmer in all_kmers]
 
                    X = np.array(features).reshape(1,-1)
-                   y_pred_proba = loaded_model.predict_proba(X)
+
+                   
+                   y_pred_proba = model_clade.predict_proba(X)
                    elements = y_pred_proba.tolist()
                    
                    for inner_list in elements:
                        clade = ''
                        if inner_list[0] > inner_list[1]:
-                           clade = 'cladeß1'
+                           clade = 'Clade 1'
+                           y_pred_proba = model_subclade.predict_proba(X)
+                           elements_subclade = y_pred_proba.tolist()
+
+                           for inner_list_subclade in elements_subclade:
+                               if inner_list_subclade[0] > inner_list_subclade[1]:
+                                   clade += 'a'
+                               else:
+                                   clade += 'b'
                        else:
-                           clade = 'clade2'
+                           clade = 'Clade 2b'
                    output.append({
-                       'ID':record.id,
-                       'Prediction':clade
+                       'id':record.id,
+                       'prediction':clade,
+                       'length':len(record.seq)
                    })
                     
-                print(output)
+                
 
                 return render(request, 'clade_predictor/results.html', 
-                              context={'output':output})
+                              context={'output':output,
+                                       'n_seqs':n_seqs})
 
     else:
         form = genomeForm()
